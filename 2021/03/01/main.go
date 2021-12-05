97d8a51b608d610fd4bb03d5a0afcc238c63da5e
Rizzi Giuseppe <giuseppe.rizzi@burda-forward.it>
2021-12-05 12:51:14 +0100

    Add day 3 part 1

---
+++ 2021/03/01/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+type counter map[byte]int
+
+func buildRates(counters []counter) ([]byte, []byte) {
+	size := len(counters)
+	gamma := make([]byte, size)
+	epsilon := make([]byte, size)
+
+	for i, c := range counters {
+		if c['0'] > c['1'] {
+			gamma[i] = '0'
+			epsilon[i] = '1'
+		} else {
+			gamma[i] = '1'
+			epsilon[i] = '0'
+		}
+	}
+
+	return gamma, epsilon
+}
+
+func convertToDecimal(bit byte, i int, size int) int {
+	return int(math.Pow(2, float64(size-i-1)) * float64(bit-'0'))
+}
+
+func computeConsumption(gamma []byte, epsilon []byte) int {
+	var g int
+	var e int
+	size := len(gamma)
+
+	for i := 0; i < size; i++ {
+		g += convertToDecimal(gamma[i], i, size)
+		e += convertToDecimal(epsilon[i], i, size)
+	}
+
+	return g * e
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var counters []counter
+	var size int
+
+	for scanner.Scan() {
+		report := scanner.Bytes()
+
+		if counters == nil {
+			size = len(report)
+			counters = make([]counter, size)
+			for i := 0; i < size; i++ {
+				counters[i] = make(map[byte]int)
+			}
+		}
+
+		for i, bit := range report {
+			counters[i][bit]++
+		}
+	}
+
+	gamma, epsilon := buildRates(counters)
+	consumption := computeConsumption(gamma, epsilon)
+
+	fmt.Println(consumption)
+}
